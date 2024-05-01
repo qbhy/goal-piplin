1164c70844c42f98c80b6936265e99009305bd49
qbhy <96qbhy@gmail.com>
2024-05-01 23:34:35 +0400

    feat: 添加部署步骤筛选功能

--- app/http/controllers/command.go
+++ app/http/controllers/command.go
@@ -10,6 +10,15 @@ func GetCommands(request contracts.HttpRequest) any {
 	list, total := models.Commands().
 		Where("project_id", request.QueryParam("project_id")).
 		OrderByDesc("id").
+		When(request.GetString("name") != "", func(q contracts.QueryBuilder[models.Command]) contracts.Query[models.Command] {
+			return q.Where("name", "like", "%"+request.GetString("name")+"%")
+		}).
+		When(request.GetString("user") != "", func(q contracts.QueryBuilder[models.Command]) contracts.Query[models.Command] {
+			return q.Where("user", "like", "%"+request.GetString("user")+"%")
+		}).
+		When(request.GetString("step") != "", func(q contracts.QueryBuilder[models.Command]) contracts.Query[models.Command] {
+			return q.Where("step", request.GetString("step"))
+		}).
 		Paginate(20, request.Int64Optional("page", 1))
 	return contracts.Fields{
 		"total": total,
