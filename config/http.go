d904ca37c852c72460a9efc7018bf0a94daef8e4
qbhy <96qbhy@gmail.com>
2024-05-08 23:30:46 +0400

    feat: 升级 http 组件，sse 支持心跳

--- config/http.go
+++ config/http.go
@@ -3,13 +3,15 @@ package config
 import (
 	"github.com/goal-web/contracts"
 	"github.com/goal-web/http"
+	"time"
 )
 
 func init() {
 	configs["http"] = func(env contracts.Env) any {
 		return http.Config{
-			Host: env.GetString("http.host"),
-			Port: env.GetString("http.port"),
+			Host:         env.GetString("http.host"),
+			Port:         env.GetString("http.port"),
+			SseHeartBeat: 15 * time.Second,
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -18,7 +18,7 @@ require (
 	github.com/goal-web/events v0.2.0
 	github.com/goal-web/filesystem v0.2.0
 	github.com/goal-web/hashing v0.2.0
-	github.com/goal-web/http v0.2.18
+	github.com/goal-web/http v0.2.19
 	github.com/goal-web/micro v0.2.0
 	github.com/goal-web/microdemo v0.0.0-20220219171937-6f3fca7dfbcb
 	github.com/goal-web/queue v0.2.1
